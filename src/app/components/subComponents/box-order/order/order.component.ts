fa9962bc7d06b1334a35e1d3d8c78edce1052421
Lucas <cordeiropinheirolucas@gmail.com>
2023-12-26 21:08:42 -0300

    adicionando as funcionalidades com a API

--- src/app/components/subComponents/box-order/order/order.component.ts
+++ src/app/components/subComponents/box-order/order/order.component.ts
@@ -1,32 +1,34 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Order, Product } from 'src/app/models/modelos';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.css']
+  styleUrls: ['./order.component.css'],
+
 })
 export class OrderComponent implements OnInit {
-  @Input() produto: any;
+  @Input() produto:  Order|any;
   @Output() valueZero = new EventEmitter<void>();
   
   ngOnInit() {
     // Garante que o valor inicial seja 1
-    if (!this.produto.quantidade || this.produto.quantidade < 1) {
-      this.produto.quantidade = 1;
+    if (!this.produto.quantity || this.produto.quantity < 1) {
+      this.produto.quantity = 1;
     }
   }
 
   decreaseValue() {
-    if (this.produto.quantidade > 0) {
-      this.produto.quantidade--;
+    if (this.produto.quantity > 0) {
+      this.produto.quantity--;
 
-      if (this.produto.quantidade === 0) {
+      if (this.produto.quantity === 0) {
         this.valueZero.emit();
       }
     }
   }
 
   increaseValue() {
-    this.produto.quantidade++;
+    this.produto.quantity++;
   }
 }
--- src/app/models/modelos.ts
+++ src/app/models/modelos.ts
@@ -1,6 +1,13 @@
-export type product ={
+export type Product ={
     id:number,
     price:number,
     categoria:string,
     name: string,
 }
+
+export type Order = {
+    id: number;
+    product: Product;
+    quantity: number;
+    price: number;
+  };
