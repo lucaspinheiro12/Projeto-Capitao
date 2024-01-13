b0420bf39a626c9e56165c8f68d3ff7718afb208
Lucas <cordeiropinheirolucas@gmail.com>
2024-01-13 16:51:36 -0300

    adicionando respostas do back end nos alerts de erros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,16 @@ import { HeaderMenuRinghtComponent } from './components/header/sub-components/he
 import { ProductsBoxRightComponent } from './components/Sales-command/main/sub-components/menu-product/products-box-right/products-box-right.component';
 import { SearchCommandComponent } from './components/Sales-command/main/sub-components/sub-header/sub-components/search-command/search-command.component';
 import { SearchNameComponent } from './components/header/sub-components/search-product/search-product.component';
-
 import { MainComponent } from './components/Sales-command/main/main.component';
 import { OrderComponent } from './components/Sales-command/main/sub-components/box-order/order/order.component'
 import { FormsModule } from '@angular/forms';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './components/register/register.component';
 import { SearchComponent } from './components/search/search.component';
 import { SummedUpComponent } from './components/search/sub-components/summed-up/summed-up.component';
 import { DetailedComponent } from './components/search/sub-components/detailed/detailed.component';
 import { BoxOrderComponent } from './components/Sales-command/main/sub-components/box-order/box-order.component';
-
 
 @NgModule({
   declarations: [
@@ -42,7 +41,7 @@ import { BoxOrderComponent } from './components/Sales-command/main/sub-component
     SearchComponent,
     SummedUpComponent,
     DetailedComponent,
-    BoxOrderComponent
+    BoxOrderComponent,
   ],  
   
   imports: [
@@ -50,7 +49,7 @@ import { BoxOrderComponent } from './components/Sales-command/main/sub-component
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    
+    SweetAlert2Module,
   ],
   providers: [],
   bootstrap: [AppComponent]
